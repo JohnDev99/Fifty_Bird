7a234b316a0ee70b8f3c6fa67b3841e329c28bc5
Marcelo <marcelogamedev99@gmail.com>
2022-12-04 19:07:52 +0000

    Flappy4

--- main.lua
+++ main.lua
@@ -31,6 +31,8 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+    --Tabela de Inputs do teclado
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -38,16 +40,28 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+
     if key == 'escape' then
         love.event.quit()
     end
 end
+
+function love.keyboard.wasPressed(Key)
+    if love.keyboard.keysPressed[Key] then
+        return true
+    else
+        return false
+    end
+end
+
 
 function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
     bird:update(dt)
+    love.keyboard.keysPressed = {}
 end 
 
 function love.draw()
