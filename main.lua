1feeb1ad91f77d06898fcba337ba2dec454d8b4c
Marcelo <marcelogamedev99@gmail.com>
2022-12-05 15:59:20 +0000

    Flappy7

--- main.lua
+++ main.lua
@@ -30,6 +30,8 @@ local pipePairs = {}
 local lastY = -PIPE_HEIGHT + math.random(80) + 20
 local spawnTimer = 0
 
+local scrolling = true
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -66,32 +68,48 @@ end
 
 
 function love.update(dt)
-    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
+    if scrolling == true then
+        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 
-    spawnTimer = spawnTimer + dt
-    if spawnTimer > 2 then
-        local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), 
-        VIRTUAL_HEIGHT -90 - PIPE_HEIGHT))
+        spawnTimer = spawnTimer + dt
+        if spawnTimer > 2 then
+            local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), 
+            VIRTUAL_HEIGHT -90 - PIPE_HEIGHT))
 
-        lastY = y
-        --Inserir na minha coleçao uma nova instancia de um objeto Instance(contructorArgms)
-        table.insert(pipePairs, PipePair(y))
-        spawnTimer = 0
+            lastY = y
+            --Inserir na minha coleçao uma nova instancia de um objeto Instance(contructorArgms)
+            table.insert(pipePairs, PipePair(y))
+            spawnTimer = 0
 
-    end
+        end
 
-    bird:update(dt)
+        bird:update(dt)
 
-    --for x, object na coleçao(key, value) faz...
-    for k, pair in pairs(pipePairs) do
-        pair:update(dt)
-    end
+        --for x, object na coleçao(key, value) faz...
+        for k, pair in pairs(pipePairs) do
+            pair:update(dt)
 
-    for k, pair in pairs(pipePairs) do
-        if pair.remove == true then
-            --remove(dacoleçao, objeto)
-            table.remove(pipePairs, k)
+            --laço for 
+            for l, pipe in pairs(pair.pipes) do
+                if bird:collides(pipe) then 
+                    scrolling = false
+                end
+            end
+
+            if pair.x < -PIPE_WIDTH then
+                pair.remove = true
+            end
+
+        end
+
+
+
+        for k, pair in pairs(pipePairs) do
+            if pair.remove == true then
+                --remove(dacoleçao, objeto)
+                table.remove(pipePairs, k)
+            end
         end
     end
 
